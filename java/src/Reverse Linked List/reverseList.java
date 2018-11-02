6d49b87afde36aa90577504af38a4c1016e459b3
kunlk <kunlk@outlook.com>
2018-11-02 17:28:29 +0800

    Update reverseList.java

--- java/src/Reverse Linked List/reverseList.java
+++ java/src/Reverse Linked List/reverseList.java
@@ -10,7 +10,7 @@ class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
 		ListNode curr = head;
-        
+		
 		while(curr != null){
 			ListNode temp = curr.next;
 			curr.next = prev;
