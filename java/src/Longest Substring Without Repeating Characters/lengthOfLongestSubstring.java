aeb2a2b06d370f5ff585ac18b967319bc6114f69
kunlk <kunlk@outlook.com>
2018-11-08 13:29:01 +0800

    Update lengthOfLongestSubstring.java

--- java/src/Longest Substring Without Repeating Characters/lengthOfLongestSubstring.java
+++ java/src/Longest Substring Without Repeating Characters/lengthOfLongestSubstring.java
@@ -42,7 +42,7 @@ class Solution {
                 i = Math.max(i,map.get(s.charAt(j))+1);
             }
             map.put(s.charAt(j),j);
-	    res = Math.max(res, j-i+1);
+	     res = Math.max(res, j-i+1);
         }
         return res;
     }
