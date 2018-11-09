d894e034581e28166642ff5db8a451b941903620
kunlk <kunlk@outlook.com>
2018-11-09 10:26:29 +0800

    Update lengthOfLongestSubstring.java

--- java/src/Longest Substring Without Repeating Characters/lengthOfLongestSubstring.java
+++ java/src/Longest Substring Without Repeating Characters/lengthOfLongestSubstring.java
@@ -48,11 +48,11 @@ class Solution {
     }
 
     public int lengthOfLongestSubstring(String s) {
-	int res = 0;
-        int[] chars = new int[128];
+	 int res = 0;
+         int[] chars = new int[128];
         
 	for(int i = 0, j = 0; j < s.length(); j++){
-            i = Math.max(i, chars[s.charAt(j)]);
+           i = Math.max(i, chars[s.charAt(j)]);
 	    res = Math.max(res, j-i+1);
 	    chars[s.charAt(j)] = j + 1;
         }
