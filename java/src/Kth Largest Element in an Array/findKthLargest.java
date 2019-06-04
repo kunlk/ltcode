0799908823097e7e8c72e8a63f4cb3a439830443
kunlk <kunlk@outlook.com>
2019-06-04 20:55:11 +0800

    Update findKthLargest.java

--- java/src/Kth Largest Element in an Array/findKthLargest.java
+++ java/src/Kth Largest Element in an Array/findKthLargest.java
@@ -1,52 +1,49 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int n = nums.length;
-        quickselect(nums, 0, n - 1, k);
+        partition(nums, 0, nums.length-1, k);
         return nums[k-1];
     }
-
-    private void quickselect(int[] nums, int start, int end, int k) {
-        if (start >= end)
+    
+    void partition(int[] nums, int start, int end, int k){
+        if(start >= end)
             return;
         
-        int pivot = median3(nums,start,end);
         int i = start;
         int j = end;
+        int pivot = median3(nums, start, end);
         
-        while (i <= j) {
-            while (nums[i] > pivot)
+        while(i <= j){
+            while(nums[i] > pivot)
                 i++;
-            
-            while (nums[j] < pivot)
+            while(nums[j] < pivot)
                 j--;
-            
-            if (i <= j)
+            if(i <= j){
                 swap(nums, i++, j--);
+            }
         }
         
-        if (k == i)
-            return;
-        if (k < i)
-            quickselect(nums, start, i - 1, k);
-        else
-            quickselect(nums, i, end, k);    
+        if(k <= i){
+            partition(nums, start, i - 1, k);
+        }else{
+            partition(nums, i, end, k);
+        }
     }
-
-    private int median3(int[] arrs, int left, int right) {
-        int center = (left + right) / 2;
-        if(arrs[center] < arrs[left])
-            swap(arrs,left,center);
-        if(arrs[right] < arrs[left])
-            swap(arrs,left,right);
-        if(arrs[right] < arrs[center])
-            swap(arrs,center,right);
-        
-        return arrs[center];
+    
+    void swap(int[] nums, int left, int right){
+        int tmp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = tmp;
     }
-
-    private void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+    
+    int median3(int[] nums, int left, int right){
+        int center = (right + left) / 2;
+        if(nums[left] < nums[center]){
+            swap(nums, left, center);
+        }else if (nums[left] < nums[right]){
+            swap(nums, left, right);
+        }else if (nums[center] < nums[right]){
+            swap(nums, center, right);
+        }
+        return nums[center];
     }
 }
